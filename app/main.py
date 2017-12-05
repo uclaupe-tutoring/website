b36ea770d9b950b0f753b03f45864a25ebd10982
Daniel Norman <danielnorman@ucla.edu>
2017-12-04 19:37:16 -0800

    Grammar and remove the class title line

--- app/main.py
+++ app/main.py
@@ -54,8 +54,6 @@ def allowed_file(filename):
 
 def generate_markdown(file_contents, language='cpp'):
   return """
-## CS31/CS32/CS33 [the class this problem is for]
-
 ### Your Problem Title
 
 *Contributed by Your Name*.
@@ -68,7 +66,7 @@ Example inputs and outputs, if helpful. If you don't have this, remove this sect
 
 #### Solution
 
-If you solution would benefit from a textual description, put that here. If you only want to provide the code, then remove these sentences!
+TODO: If your solution would benefit from a textual description, put that here. If you only want to provide the code, then remove these sentences!
 
 ```%s
 %s
